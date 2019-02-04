a12a58619dac082eaf81a3d739c10ca248ac652d
Paul Millar <paul.millar@desy.de>
2019-02-04 11:41:13 +0100

    Separate SOUND from PROLEMATIC endpoints in output

--- bin/smoke-em.sh
+++ bin/smoke-em.sh
@@ -3,13 +3,31 @@
 #  Run smoke-test.sh on all endpoints
 
 BASE=$(cd $(dirname $0)/..;pwd)
-OUTPUT=$(mktemp)
+SMOKE_OUTPUT=$(mktemp)
+RESULTS=$(mktemp)
 
-rm -f results
+SOUND_ENDPOINT_RE="0 failed, 0 skipped"
+
 cat $BASE/etc/sites | while read site url; do
-    bin/smoke-test.sh -f $url > $OUTPUT
-    echo -e "$site\t$(tail -1 $OUTPUT)" >> results
+    bin/smoke-test.sh -f $url > $SMOKE_OUTPUT
+    echo -e "$site\t$(tail -1 $SMOKE_OUTPUT)" >> $RESULTS
 done
-rm -f $OUTPUT
 
-column -t results -s $'\t'
+column -t $RESULTS -s $'\t' > $SMOKE_OUTPUT
+rm $RESULTS
+
+if grep -q "$SOUND_ENDPOINT_RE" $SMOKE_OUTPUT; then
+    echo
+    echo "SOUND ENDPOINTS"
+    echo
+    grep "$SOUND_ENDPOINT_RE" $SMOKE_OUTPUT
+fi
+
+if grep -v -q "$SOUND_ENDPOINT_RE" $SMOKE_OUTPUT; then
+    echo
+    echo "PROBLEMATIC ENDPOINTS"
+    echo
+    grep -v "$SOUND_ENDPOINT_RE" $SMOKE_OUTPUT
+fi
+
+rm $SMOKE_OUTPUT
