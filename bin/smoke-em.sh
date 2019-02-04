b1da53f57a38c43c24063ce2d849a6551b3751cf
Paul Millar <paul.millar@desy.de>
2019-02-04 11:55:38 +0100

    Rename endpoints to the name used within Rucio; add storage type as name may be insufficient.

--- bin/smoke-em.sh
+++ bin/smoke-em.sh
@@ -8,9 +8,9 @@ RESULTS=$(mktemp)
 
 SOUND_ENDPOINT_RE="0 failed, 0 skipped"
 
-cat $BASE/etc/endpoints | while read name url; do
+cat $BASE/etc/endpoints | while read name type url; do
     bin/smoke-test.sh -f $url > $SMOKE_OUTPUT
-    echo -e "$name\t$(tail -1 $SMOKE_OUTPUT)" >> $RESULTS
+    echo -e "$name\t$type\t$(tail -1 $SMOKE_OUTPUT)" >> $RESULTS
 done
 
 column -t $RESULTS -s $'\t' > $SMOKE_OUTPUT
